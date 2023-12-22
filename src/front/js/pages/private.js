8622adf73bd7e93d52e4e1f1b3cb58916b690614
JoseRegueiro17 <132882721+JoseRegueiro17@users.noreply.github.com>
2023-12-22 14:48:48 +0000

    refactoring!

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,6 +1,7 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, Componen } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/cohete.png";
+import ReactPlayer from 'react-player'
 
 
 
@@ -12,6 +13,23 @@ export const Private = () => {
         <div className="text-center mt-5">
             <h1>Bienvenido <b>{store.email}</b> esta es tu pagina privada.</h1>
         </div>
+
+
+        <div>
+        <ReactPlayer
+          url='https://www.youtube.com/watch?v=gtckSqaXCk8'
+          className='react-player'
+          playing={false}
+          width='100%'
+          height='100%'/>
+         </div>
+
+         <div className="text-center mt-5">
+            <h2>Donde guardas las canciones que te encantan pero, no se las muestras a nadie</h2>
+        </div>
+
+
+        
 
         </>
     );
